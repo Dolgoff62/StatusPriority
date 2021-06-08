746f2282f888a6da54768bae0707fe567c9def68
Dmitriy Dolgov <dolgov2007@inbox.ru>
2021-06-08 08:31:27 +0300

    alternative variant of priority status fun

--- app/src/main/java/ru/netology/statuspriority/Utils.kt
+++ app/src/main/java/ru/netology/statuspriority/Utils.kt
@@ -6,39 +6,67 @@ import java.time.temporal.ChronoUnit
 
 class Utils {
     companion object {
+//        fun determiningPriorityLevelOfNote(planeDateTime: LocalDateTime) : StatusPriority {
+//
+//            val hours: Long = ChronoUnit.MINUTES.between(LocalDateTime.now(), planeDateTime)
+//            if (hours <= 120) {
+//                return StatusPriority.RED
+//            }
+//
+//            val nowPlusTwoHours = LocalDateTime.now().plusHours(2)
+//            val nextSixOClock: LocalDateTime
+//
+//            if(LocalDateTime.now().hour < 6) {
+//                nextSixOClock = LocalDateTime.of(
+//                    LocalDate.now().year,
+//                    LocalDate.now().monthValue,
+//                    LocalDate.now().dayOfMonth,
+//                    6,
+//                    0
+//                    )
+//            } else {
+//                nextSixOClock = LocalDateTime.of(
+//                    LocalDate.now().year,
+//                    LocalDate.now().monthValue,
+//                    LocalDate.now().plusDays(1).dayOfMonth,
+//                    6,
+//                    0
+//                )
+//            }
+//
+//            return if (nowPlusTwoHours.isBefore(planeDateTime) && planeDateTime.isBefore(nextSixOClock)) {
+//                StatusPriority.YELLOW
+//            } else {
+//                StatusPriority.GREEN
+//            }
+//        }
+
+
         fun determiningPriorityLevelOfNote(planeDateTime: LocalDateTime) : StatusPriority {
 
-            val hours: Long = ChronoUnit.MINUTES.between(LocalDateTime.now(), planeDateTime)
-            if (hours <= 120) {
+            val now = LocalDateTime.now()
+            val mins: Long = ChronoUnit.MINUTES.between(now, planeDateTime)
+            if (mins <= 120) {
                 return StatusPriority.RED
             }
 
-            val nowPlusTwoHours = LocalDateTime.now().plusHours(2)
-            val nextSixOClock: LocalDateTime
+            val nextSixOClock = LocalDateTime.of(
+            LocalDate.now().year,
+            LocalDate.now().monthValue,
+            LocalDate.now().dayOfMonth,
+            6,
+            0
+            )
 
-            if(LocalDateTime.now().hour < 6) {
-                nextSixOClock = LocalDateTime.of(
-                    LocalDate.now().year,
-                    LocalDate.now().monthValue,
-                    LocalDate.now().dayOfMonth,
-                    6,
-                    0
-                    )
-            } else {
-                nextSixOClock = LocalDateTime.of(
-                    LocalDate.now().year,
-                    LocalDate.now().monthValue,
-                    LocalDate.now().plusDays(1).dayOfMonth,
-                    6,
-                    0
-                )
+            if(now.hour > 6) {
+                nextSixOClock.plusDays(1)
             }
 
-            return if (nowPlusTwoHours.isBefore(planeDateTime) && planeDateTime.isBefore(nextSixOClock)) {
-                StatusPriority.YELLOW
-            } else {
-                StatusPriority.GREEN
+            if ( mins > 120 && planeDateTime.isBefore(nextSixOClock)) {
+                return StatusPriority.YELLOW
             }
+
+            return StatusPriority.GREEN
         }
     }
 }
